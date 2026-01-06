78cdf18c2530a4238cb603587666a9fcaa1e1fa5
TheColGateMann4 <TheColGateMann4@gmail.com>
2026-01-06 19:37:46 +0100

    Added simple GOP start point
    Printing Screen Resolution
    
    Normalized indents

--- src/bootloader/EntryPoint.cpp
+++ src/bootloader/EntryPoint.cpp
@@ -1,29 +1,40 @@
 #include <efi.h>
 #include <efilib.h>
 
-constexpr int a()
-{
-	return 1;
-}
-
 extern "C" int _fltused = 0;
 
 extern "C"
 EFI_STATUS efi_main(
-    EFI_HANDLE        ImageHandle,
-    EFI_SYSTEM_TABLE* SystemTable
-)
+		EFI_HANDLE        ImageHandle,
+		EFI_SYSTEM_TABLE* SystemTable
+		)
 {
-   InitializeLib(ImageHandle, SystemTable);
+	InitializeLib(ImageHandle, SystemTable);
 
-    AsciiPrint((CHAR8*)"Hello from ThyraOS!\n");
-    AsciiPrint((CHAR8*)"Hello from ThyraOS!\n");
-    AsciiPrint((CHAR8*)"Hello from ThyraOS!\n");
+	AsciiPrint((CHAR8*)"Hello from ThyraOS!\n");
 
-    while (true) 
-    {
-	    __asm__ __volatile__("hlt");
-    }
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop = nullptr;
 
-    return EFI_SUCCESS;
+	EFI_GUID graphicsOutputProtocolGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	SystemTable->BootServices->LocateProtocol(
+		&graphicsOutputProtocolGuid,
+		nullptr,
+		(void**)&gop
+	);
+
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE* gopMode = gop->Mode;
+	EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* gopInformation = gopMode->Info;
+
+	UINT32 horizontalResolution = gopInformation->HorizontalResolution;
+	UINT32 verticalResolution = gopInformation->VerticalResolution;
+
+
+	AsciiPrint((CHAR8*)"Screen Resolution is %dx%d", horizontalResolution, verticalResolution);
+
+	while (true) 
+	{
+		__asm__ __volatile__("hlt");
+	}
+
+	return EFI_SUCCESS;
 }
